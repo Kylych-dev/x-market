efa17675b7dfc416de18be05bc0fdb5a8a472d45
Mirbekov <@kylych-bob>
2023-07-16 13:52:32 +0600

    make orders

--- app/orders/models.py
+++ app/orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 
-# Create your models here.
+from . import managers
+
+
+class Order(models.Model):
+    STATUS = (
+        ('R','received'),
+        ('P','posted'),
+        ('N','not posted'),
+    )
+
+    owner = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='orders'
+    )
+    # product = models.ForeignKey(
+    #     to=models.
+    # )
+
+
+    total_price = models.PositiveIntegerField(default=0)

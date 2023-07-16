43a8b5534070d2192f663196fd6b138ed3beea02
Kylych <69972051+Kylych-dev@users.noreply.github.com>
2023-07-16 15:09:27 +0600

    Revert "Dev2"

--- app/product/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Product
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price', 'category']
-    search_fields = ['title', 'category']
